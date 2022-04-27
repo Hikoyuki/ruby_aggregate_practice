a7c55ddaa675ab8bf902d57e744686493fc1b48c
酒井彦行 <hikoyuki.sakai@gmail.com>
2022-04-27 21:36:58 +0900

    rubocop -a

--- aggregator/high_motivation_user_aggregator.rb
+++ aggregator/high_motivation_user_aggregator.rb
@@ -10,13 +10,13 @@ class HighMotivationUserAggregator
   # 実装してください
   def exec
     best_channel = []
-    channel_names.each.with_index(1) do |channel, i|
+    channel_names.each.with_index(1) do |channel, _i|
       slack_data = load(channel)
-      tmp_hash = { channel: channel, message_count: slack_data['messages'].size}
+      tmp_hash = { channel: channel, message_count: slack_data['messages'].size }
       best_channel << tmp_hash
     end
     # メッセージ数の降順で並び替え
-    best_channel.sort_by!{ |k| -k[:message_count] }
+    best_channel.sort_by! { |k| -k[:message_count] }
     # 上位3つを取得
     best_channel.slice(0, 3)
   end
