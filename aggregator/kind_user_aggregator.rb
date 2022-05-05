54f07aafd782de59f8dda791c3a9f07ec5d13016
酒井彦行 <hikoyuki.sakai@gmail.com>
2022-05-06 01:54:03 +0900

    fix task2

--- aggregator/kind_user_aggregator.rb
+++ aggregator/kind_user_aggregator.rb
@@ -10,22 +10,27 @@ class KindUserAggregator
   # 実装してください
   def exec
     slack_data = channel_names.map do |channel|
-      data = load(channel)
+      load(channel)
     end
 
     message_data = slack_data.map do |data|
       message = data['messages']
-      message.map do |ms|
+      message_reaction = message.map do |ms|
         ms.select {|k,v| k == 'reactions' }
       end.flatten
     end.flatten
 
     message_reaction = message_data.delete_if {|md| md == {} }
 
-    message_reaction_data = message_reaction.map do |mr|
+    reaction_users = message_reaction.flat_map do |mr|
+      mr['reactions'][0]['users']
+    end
+    sum_user = reaction_users.group_by(&:itself).map{|k, v| [k, v.size]}.to_h
+
+    message_reaction_data = sum_user.map do |sm|
       {
-        user_id: mr['reactions'][0]['users'],
-        reaction_count: mr['reactions'][0]['count']
+        user_id: sm.first,
+        reaction_count: sm.last
       }
     end
 
